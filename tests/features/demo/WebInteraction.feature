4904c79513ebeaa0a14374cb2883bf00ed95e8c4
MohamedAmeerkhan <mohamedameerkhank@gmail.com>
2023-06-17 07:22:40 +0530

    first

---
+++ tests/features/demo/WebInteraction.feature
@@ -0,0 +1,15 @@
+Feature: WebInteraction
+
+    # @demo1
+    # Scenario Outline: Orange HRM Applications
+    # Given I open orange HRM 
+    # When I enter correct credentials
+    # Then I see orange HRM web application opened
+
+    @demo2
+    Scenario: The Internet Herokuapp
+    Given I launch the Internet Herokuapp
+    When I enter number in the input field
+    Then I clear the input field and type slowly
+
+   
--- tests/features/step-definitions/demo.ts
+++ tests/features/step-definitions/demo.ts
@@ -1,4 +1,4 @@
-import { Given,Then,When } from "@wdio/cucumber-framework";
+import { Given,Then,When, } from "@wdio/cucumber-framework";
 import chai from "chai"
 Given(/^Google page is opened$/,async function(){
     await console.log("Before opening the chrome");
@@ -23,3 +23,42 @@ Then(/^URL should match (.*)$/,async function(ExpectedURL){
     console.log(`>>Expected URL: ${ExpectedURL}`);
     await expect(browser).toHaveUrl('https://webdriver.io/') 
 })
+Given(/^I open orange HRM$/,async function(){
+    await browser.url('/')
+    await browser.maximizeWindow()
+    await browser.pause(10000)
+    await browser.setTimeout({implicit:15000,pageLoad:10000})
+})
+When(/^I enter correct credentials$/,async function(){
+    await $('input[name="username"]').setValue('Admin')
+     await $('input[name="password"]').setValue('admin123')
+     await $('button[type="submit"]').click()
+})
+Then(/^I see orange HRM web application opened$/,async function(){
+    await browser.pause(10000)
+    await expect(browser).toHaveUrlContaining('https://opensource-demo.orangehrmlive.com/')
+})
+Given(/^I launch the Internet Herokuapp$/,async function(){
+    await browser.url('https://the-internet.herokuapp.com/inputs')
+})
+When(/^I enter number in the input field$/,async function(){
+    let number=12345
+    let string=number.toString()
+    let set=await $('input[type="number"]')
+    set.setValue(string)
+    await browser.pause(10000)
+    await $('input[type="number"]').clearValue()
+})
+Then(/^I clear the input field and type slowly$/,async function(){
+    let number1=12345
+    let string1=number1.toString()
+    let set1=await $('input[type="number"]')
+    set1.click()
+   for(let i=0;i<=string1.length;i++)
+   {
+    let iteration=string1.charAt(i)
+    await browser.keys(iteration)
+   }
+ await browser.pause(8000)
+})
+
